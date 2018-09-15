6e7a7db144a2f789e01e6c36da3c6e0e6da44c37
syarifhidayatullah <syarif.h@go-jek.com>
2018-09-15 22:35:10 +0700

    Updated Main pieline with fixed window to transform unbounded to bounded

--- src/main/java/com/saungkertas/backyard/MainPipeline.java
+++ src/main/java/com/saungkertas/backyard/MainPipeline.java
@@ -5,10 +5,19 @@ import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.windowing.AfterWatermark;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
+import org.joda.time.Duration;
 
 public class MainPipeline {
 
     public static void main(String[] args) {
+
+        Window<String> window = Window.<String>into(FixedWindows.of(Duration.standardMinutes(1)))
+                .triggering(AfterWatermark.pastEndOfWindow())
+                .discardingFiredPanes()
+                .withAllowedLateness(Duration.standardMinutes(5));
 
         PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
         Pipeline p = Pipeline.create(options);
@@ -16,6 +25,7 @@ public class MainPipeline {
         p.apply("Read Booking from Pubsub", PubsubIO.readStrings()
                 .fromSubscription("your_pubsub_subscription")
                 .withTimestampAttribute("timestamp_value"))
+                .apply(window)
                 .apply(TextIO.write().to("your_downstream")
                         .withoutSharding()
                         .withSuffix(".txt")
